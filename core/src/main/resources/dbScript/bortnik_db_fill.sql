9711d67e40ca692184aa0b39e7ad7a8212540dee
dashabortnik <soleildasha@gmail.com>
2019-07-15 19:46:49 +0300

    Updated SQL files, removing hardcoded id values

--- core/src/main/resources/dbScript/bortnik_db_fill.sql
+++ core/src/main/resources/dbScript/bortnik_db_fill.sql
@@ -21,48 +21,48 @@ VALUES ('Zinina', 'Karina','Andreevna', '1985/07/05', 'female', 'belarusian', 'd
 /*insert address*/
 
 INSERT INTO address (country, city, street, postcode, contact_id) 
-VALUES ('Russia', 'Moscow','8 Marta 10-5', '117623', 1);
+VALUES ('Russia', 'Moscow','8 Marta 10-5', '117623', (SELECT contact_id FROM contact WHERE surname = 'Petrov' AND name = 'Ivan'));
 
 INSERT INTO address (country, city, street, postcode, contact_id) 
-VALUES ('Belarus', 'Minsk','Dvinskaya 15-23', '220037', 2);
+VALUES ('Belarus', 'Minsk','Dvinskaya 15-23', '220037', (SELECT contact_id FROM contact WHERE surname = 'Komarov' AND name = 'Pavel'));
 
 INSERT INTO address (country, city, street, postcode, contact_id) 
-VALUES ('Russia', 'Moscow','Taganskaya 34A', '109147', 3);
+VALUES ('Russia', 'Moscow','Taganskaya 34A', '109147', (SELECT contact_id FROM contact WHERE surname = 'Lodochkin' AND name = 'Yuri'));
 
 INSERT INTO address (country, city, street, postcode, contact_id) 
-VALUES ('USA', 'New York','247 Cessna Drive', '46818', 4);
+VALUES ('USA', 'New York','247 Cessna Drive', '46818', (SELECT contact_id FROM contact WHERE surname = 'Solnceva' AND name = 'Irina'));
 
 INSERT INTO address (country, city, street, postcode, contact_id) 
-VALUES ('Russia', 'SPB','Sedova 11', '192019', 5);
+VALUES ('Russia', 'SPB','Sedova 11', '192019', (SELECT contact_id FROM contact WHERE surname = 'Kalinina' AND name = 'Maria'));
 
 INSERT INTO address (country, city, street, postcode, contact_id) 
-VALUES ('Belarus', 'Mogilev','Minina 13-1', '212037', 6);
+VALUES ('Belarus', 'Mogilev','Minina 13-1', '212037', (SELECT contact_id FROM contact WHERE surname = 'Zinina' AND name = 'Karina'));
 
 /*insert phone*/
 
 INSERT INTO phone (country_code, operator_code, phone_number, phone_type, COMMENT, contact_id) 
-VALUES ('+7', '495','7556983', 'home', NULL, 1);
+VALUES ('+7', '495','7556983', 'home', NULL, (SELECT contact_id FROM contact WHERE surname = 'Petrov' AND name = 'Ivan'));
 
 INSERT INTO phone (country_code, operator_code, phone_number, phone_type, COMMENT, contact_id) 
-VALUES ('+375', '29','3278567', 'mobile', NULL, 2);
+VALUES ('+375', '29','3278567', 'mobile', NULL, (SELECT contact_id FROM contact WHERE surname = 'Komarov' AND name = 'Pavel'));
 
 INSERT INTO phone (country_code, operator_code, phone_number, phone_type, COMMENT, contact_id) 
-VALUES ('+7', '499','7345465', 'home', NULL, 3);
+VALUES ('+7', '499','7345465', 'home', NULL, (SELECT contact_id FROM contact WHERE surname = 'Lodochkin' AND name = 'Yuri'));
 
 INSERT INTO phone (country_code, operator_code, phone_number, phone_type, COMMENT, contact_id) 
-VALUES ('+7', '926','4564778', 'mobile', NULL, 3);
+VALUES ('+7', '926','4564778', 'mobile', NULL, (SELECT contact_id FROM contact WHERE surname = 'Lodochkin' AND name = 'Yuri'));
 
 INSERT INTO phone (country_code, operator_code, phone_number, phone_type, COMMENT, contact_id) 
-VALUES ('1', '212','3482626', 'home', NULL, 4);
+VALUES ('1', '212','3482626', 'home', NULL, (SELECT contact_id FROM contact WHERE surname = 'Solnceva' AND name = 'Irina'));
 
 INSERT INTO phone (country_code, operator_code, phone_number, phone_type, COMMENT, contact_id) 
-VALUES ('+7', '903','5467667', 'mobile', NULL, 5);
+VALUES ('+7', '903','5467667', 'mobile', NULL, (SELECT contact_id FROM contact WHERE surname = 'Kalinina' AND name = 'Maria'));
 
 INSERT INTO phone (country_code, operator_code, phone_number, phone_type, COMMENT, contact_id) 
-VALUES ('+375', '33','3455654', 'mobile', NULL, 6);
+VALUES ('+375', '33','3455654', 'mobile', NULL, (SELECT contact_id FROM contact WHERE surname = 'Zinina' AND name = 'Karina'));
 
 /*insert attachment*/
 INSERT INTO attachment (attachment_name, attachment_link, upload_date, contact_id) 
-VALUES ('passport', 'https://clck.ru/GP4ri', CURDATE(), 1);
+VALUES ('passport', 'https://clck.ru/GP4ri', CURDATE(), (SELECT contact_id FROM contact WHERE surname = 'Petrov' AND name = 'Ivan'));
 
 
